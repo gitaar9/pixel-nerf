1380d4ad94d708c8113dbfb1174255f96fef5c3e
gitaar9 <thijs.eker@gmail.com>
2021-06-15 14:01:30 +0200

    eval jobscript

---
+++ eval/ssim_psnr_from_finish.py
@@ -0,0 +1,18 @@
+import os
+
+import numpy as np
+
+
+def read_file(folder_path):
+    with open(os.path.join(folder_path, 'finish.txt'), 'r') as f:
+        lines = f.readlines()
+    n = [list(map(float, l.strip().split()[1:3])) for l in lines]
+    return np.asarray(n)
+
+
+folder = '/samsung_hdd/Files/AI/TNO/remote_folders/pixel_nerf_eval/pixel_nerf_eval_output/sncar'
+nm = read_file(os.path.join(folder, 'no_mirror'))
+sl = read_file(os.path.join(folder, 'sym_loss'))
+
+print(f"No mirror: {np.mean(nm, axis=0)}")
+print(f"Sym loss: {np.mean(sl, axis=0)}")
