02efe1140263f09831614e26fec3a36090e816c5
gitaar9 <thijs.eker@gmail.com>
2021-06-17 11:06:53 +0200

    generate no mirror data augmentation

--- eval/ssim_psnr_from_finish.py
+++ eval/ssim_psnr_from_finish.py
@@ -11,8 +11,9 @@ def read_file(folder_path):
 
 
 folder = '/samsung_hdd/Files/AI/TNO/remote_folders/pixel_nerf_eval/pixel_nerf_eval_output/sncar'
+folder = '/samsung_hdd/Files/AI/TNO/remote_folders/pixel_nerf_eval/pixel_nerf_eval_output/snship'
 nm = read_file(os.path.join(folder, 'no_mirror'))
 sl = read_file(os.path.join(folder, 'sym_loss'))
 
-print(f"No mirror: {np.mean(nm, axis=0)}")
-print(f"Sym loss: {np.mean(sl, axis=0)}")
+print(f"No mirror: {np.mean(nm, axis=0)} over {len(nm)} objects")
+print(f"Sym loss: {np.mean(sl, axis=0)} over {len(sl)} objects")
